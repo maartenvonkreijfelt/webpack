1409b7f5672579e5b745dfc93232a2cb26ae4e84
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2019-08-26 13:23:39 +0200

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         filename: "bundle.js",
         path: path.join(__dirname, "dist")
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
 }
